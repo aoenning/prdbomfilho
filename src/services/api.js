d5d459ed2af87053b100057195cf9022a1edbe9c
aoenning <and.oenning@gmail.com>
2021-10-24 18:18:15 -0400

    gitIgnore

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://server-system-prdbomfilho.herokuapp.com'
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
